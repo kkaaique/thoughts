a201d1ecec11ab7c3df82536a1a4e6bf6c621f97
Kaique Fernandes <fernandeskaaique@gmail.com>
2024-10-28 12:42:33 -0300

    feat: updating deleting

--- controllers/ThoughtsController.js
+++ controllers/ThoughtsController.js
@@ -24,7 +24,12 @@ module.exports = class ThoughtsController {
 
         const thoughts = user.Thoughts.map((result) => result.dataValues)
 
-        res.render('thoughts/dashboard', { thoughts })
+        let emptyThoughts = false
+
+        if (thoughts.length === 0) {
+            emptyThoughts = true
+        }
+        res.render('thoughts/dashboard', { thoughts, emptyThoughts })
     }
 
     static createThought(req, res) {
@@ -50,4 +55,46 @@ module.exports = class ThoughtsController {
         }
     }
 
+    static async removeThought(req, res) {
+        const id = req.body.id
+        const userId = req.session.userId
+
+        try {
+            await Thought.destroy({ where: { id: id, UserId: userId } })
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static async updateThought(req, res) {
+        const id = req.params.id 
+
+        const thought = await Thought.findOne({ where: { id: id }, raw: true })
+
+        res.render('thoughts/edit', { thought })
+    }
+
+    static async updateThoughtSave(req, res) {
+        const id = req.body.id
+        
+        const thought = {
+            title: req.body.title,
+        }
+
+        try {
+            await Thought.update(thought, { where: { id: id }})
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -6,7 +6,10 @@ const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/add', checkAuth, ThoughtsController.createThought)
 router.post('/add', checkAuth, ThoughtsController.postThought)
+router.get('/edit/:id', checkAuth, ThoughtsController.updateThought)
+router.post('/edit/', checkAuth, ThoughtsController.updateThoughtSave)
 router.get('/dashboard', checkAuth, ThoughtsController.dashboard)
+router.post('/remove', checkAuth, ThoughtsController.removeThought)
 router.get('/', ThoughtsController.showThoughts)
 
 module.exports = router
